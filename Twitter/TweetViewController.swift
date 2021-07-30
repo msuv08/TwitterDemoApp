6999a7c2104f5eff082cbfd695cb6138afd76c0e
Mihir Suvarna <49384703+msuv08@users.noreply.github.com>
2021-07-29 23:06:11 -0500

    Added dynamic character count for composing tweets (limit 140)

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -8,20 +8,25 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var charCountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tweetTextView.delegate = self
+        tweetTextView.layer.borderColor = UIColor.lightGray.cgColor
+        tweetTextView.layer.borderWidth = 1.0
+        tweetTextView.layer.cornerRadius = 8
         tweetTextView.becomeFirstResponder()
+        charCountLabel.text = String(0)
         // Do any additional setup after loading the view.
     }
     
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
     
     @IBAction func tweet(_ sender: Any) {
         if (!tweetTextView.text.isEmpty) {
@@ -34,6 +39,20 @@ class TweetViewController: UIViewController {
         } else {
             self.dismiss(animated: true, completion: nil)
         }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        // Set the max character limit
+        let characterLimit = 140
+
+        // Construct what the new text would be if we allowed the user's latest edit
+        let newText = NSString(string: textView.text!).replacingCharacters(in: range, with: text)
+
+        // TODO: Update Character Count Label
+        charCountLabel.text = String(newText.count)
+        // The new text should be allowed? True/False
+        return newText.count < characterLimit
+        
     }
     /*
     // MARK: - Navigation
